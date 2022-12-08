6514e336d5722009ffbf7195ffc35f330d997de2
Ethan Shealey <ethan.shealey@gmail.com>
2022-12-07 19:20:41 -0500

    add project

---
+++ yaml-converter.py
@@ -0,0 +1,16 @@
+
+current = []
+with open('yaml-in.txt', 'r') as file:
+    for line in file:
+        line = line.rstrip().replace('    ', '\t').split('\t')
+        if len(line) == 1:
+            if '#' in line[0]:
+                print('\n' + line[0])
+            else:
+                current = line
+
+        else:
+            current = current[:len(line) - 1]
+            current.append(line[-1])
+            if ': ' in '.'.join(current).replace(':.', '.'):
+                print('.'.join(current).replace(':.', '.'))
