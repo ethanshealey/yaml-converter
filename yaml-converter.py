e3a77332bf15257271be680f847fa1cc168a6b06
Ethan Shealey <ethan.shealey@gmail.com>
2022-12-07 21:30:00 -0500

    updating yaml-converter.py

--- yaml-converter.py
+++ yaml-converter.py
@@ -1,23 +1,9 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('-c', '--comments', help='include comments', action='store_true')
-
-args = parser.parse_args()
-
-print(args)
-
 current = []
 with open('yaml-in.txt', 'r') as file:
     for line in file:
         line = line.rstrip().replace('    ', '\t').split('\t')
         if len(line) == 1:
-            if '#' in line[0] and args.comments:
-                print('\n' + line[0])
-            else:
-                current = line
-
+             current = line
         else:
             current = current[:len(line) - 1]
             current.append(line[-1])
