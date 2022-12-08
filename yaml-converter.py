813b5bf54f11293a01f1416acd094b750cd5ad19
Ethan Shealey <ethan.shealey@gmail.com>
2022-12-07 19:27:05 -0500

    add argument to add or hide comments

--- yaml-converter.py
+++ yaml-converter.py
@@ -1,10 +1,19 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-c', '--comments', help='include comments', action='store_true')
+
+args = parser.parse_args()
+
+print(args)
 
 current = []
 with open('yaml-in.txt', 'r') as file:
     for line in file:
         line = line.rstrip().replace('    ', '\t').split('\t')
         if len(line) == 1:
-            if '#' in line[0]:
+            if '#' in line[0] and args.comments:
                 print('\n' + line[0])
             else:
                 current = line
